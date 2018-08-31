57b7b531762d408c4b9d325b2df7ea1f8c56f7b8
tinkidinki <mahathi98@gmail.com>
2018-08-31 19:13:11 +0530

    pwd skeleton done as well

--- turtle.c
+++ turtle.c
@@ -23,12 +23,28 @@ int display_prompt(){
 }
 
 int cd(char **args){
-    printf("cd function called");
+    printf("cd function called\n");
+
+    // if (strcpy(args[1], ".")==0){
+    //     //
+    // }
+
     if (chdir(args[1]) != 0){
         perror("turtle: ");
     }
 
     return 1;
+}
+
+int pwd(){
+    char directory[PATH_MAX];
+    printf ("pwd called\n");
+    if (getcwd(directory, sizeof(directory))!=NULL){
+        printf("%s\n", directory);
+    }
+    else{
+        perror("getcwd() error");
+    }
 }
 
 int turtle_execute(char **args){
@@ -38,9 +54,14 @@ int turtle_execute(char **args){
     }
     
     else if (strcmp(args[0], "cd")==0){
+        printf("Args[0] contents:%s\n", args[0]);
         cd(args);
-        printf ("comes back here?");
+        printf ("comes back here?\n");
         return 1;
+    }
+
+    else if (strcmp(args[0], "pwd")==0){
+        pwd();
     }
 }
 
@@ -97,7 +118,7 @@ int turtle_loop(){
         line = turtle_read_line();
         printf("line: %s\n", line);
         args = turtle_split_line(line);
-        printf("%s,%s", args[0], args[1]);        
+        //printf("%s,%s", args[0], args[1]);        
         status = turtle_execute(args);
     } while(status);
 
