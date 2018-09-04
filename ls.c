46874f01ed68782461dc815e300de17fdd00e007
tinkidinki <mahathi98@gmail.com>
2018-09-04 19:41:22 +0530

    Somewhat done first four specs

--- ls.c
+++ ls.c
@@ -21,17 +21,19 @@ void print_perms(mode_t st) {
     else if(st && S_ISCHR(st)) perms[0]='c';
     else if(st && S_ISBLK(st)) perms[0]='b';
     else perms[0]='l';  // S_ISLNK
-    perms[1] = (st && S_IRUSR) ? 'r':'-';
-    perms[2] = (st && S_IWUSR) ? 'w':'-';
-    perms[3] = (st && S_IXUSR) ? 'x':'-';
-    perms[4] = (st && S_IRGRP) ? 'r':'-';
-    perms[5] = (st && S_IWGRP) ? 'w':'-';
-    perms[6] = (st && S_IXGRP) ? 'x':'-';
-    perms[7] = (st && S_IROTH) ? 'r':'-';
-    perms[8] = (st && S_IWOTH) ? 'w':'-';
-    perms[9] = (st && S_IXOTH) ? 'x':'-';
+    perms[1] = (st & S_IRUSR) ? 'r':'-';
+    perms[2] = (st & S_IWUSR) ? 'w':'-';
+    perms[3] = (st & S_IXUSR) ? 'x':'-';
+    perms[4] = (st & S_IRGRP) ? 'r':'-';
+    perms[5] = (st & S_IWGRP) ? 'w':'-';
+    perms[6] = (st & S_IXGRP) ? 'x':'-';
+    perms[7] = (st & S_IROTH) ? 'r':'-';
+    perms[8] = (st & S_IWOTH) ? 'w':'-';
+    perms[9] = (st & S_IXOTH) ? 'x':'-';
     perms[10] = '\0';
     printf("%s", perms);
+
+    // printf("User has read: %d\n", S_IXUSR);
 }
 
 
@@ -54,12 +56,16 @@ int ls(char **args){
     }
 
     struct dirent *de;
-
     DIR *dr = opendir(dir_name);
 
+    // file name
+    char short_name[1000];
+    char name[1000];
+    char buf[128];
+
     if (dr == NULL){
-        printf("Could not open current directory");
-        return 0;
+        //printf("turtle: Could not open the directory\n");
+        return 1;
     }
 
     int l=0, a=0;
@@ -81,18 +87,57 @@ int ls(char **args){
     printf("l value:%d\n", l);
     printf("a value:%d\n", a);
 
-    if (l){
-        struct stat my_stat;
-        struct tm lt;
-        struct passwd *pwd;
-        
-    }
+    struct stat my_stat;
+    struct tm local_time;
+    struct passwd pwent, *pwentpt;
+    struct group grp, *grpt;
+    char datestring[256];
 
     while ((de = readdir(dr))!= NULL){
-        if (de->d_name[0]=='.' && !a)
+        
+        strcpy(short_name, de->d_name);
+        realpath(short_name, name);
+        //printf(" full path before: %s\n", name);
+        
+        if (short_name[0]=='.' && !a)
             continue;
 
-        printf("%s\n", de->d_name);
+        if (l){
+            stat(name, &my_stat);
+            print_perms(my_stat.st_mode);
+            printf("%d", (int) my_stat.st_nlink);
+
+            //printf(" full path middle 0: %s\n", name);
+
+            if (!getpwuid_r(my_stat.st_uid, &pwent, buf, sizeof(buf), &pwentpt)){
+                printf(" %s", pwent.pw_name);
+            }
+            else{
+                printf(" %d", my_stat.st_uid);
+            }
+
+            //printf(" full path middle 1: %s\n", name);
+
+            if (!getgrgid_r(my_stat.st_gid, &grp, buf, sizeof(buf), &grpt)){
+                printf(" %s", grp.gr_name);
+            }
+            else{
+            printf(" %d", my_stat.st_gid);
+            }
+
+            printf(" %5d", (int)my_stat.st_size);
+
+            //printf(" full path middle 2: %s\n", name);
+
+            localtime_r(&my_stat.st_mtime, &local_time);
+
+            strftime(datestring, sizeof(datestring), "%F %T", &local_time);
+
+            printf(" %s", datestring);
+        }
+
+
+        printf(" %s\n", short_name);
     }
 
     return 1;
